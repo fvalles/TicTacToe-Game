d86e592828cd9b2491f2cc88c9d335465eb61126
Federico Valles <fedeva_37@hotmail.com>
2020-02-18 17:30:39 -0300

    Lifting State Up, Again

--- tic-tac-toe/src/app/screens/Game/components/Board/index.js
+++ tic-tac-toe/src/app/screens/Game/components/Board/index.js
@@ -1,31 +1,12 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import Square from '../Square';
 
 import styles from './styles.module.scss';
 
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
-
 class Board extends Component {
-  state = {
+  /* state = {
     squares: Array(9).fill(null),
     xIsNext: true
   };
@@ -39,29 +20,21 @@ class Board extends Component {
         xIsNext: !prevState.xIsNext
       };
     });
-  }
+  }*/
 
   renderSquare(i) {
     return (
       <Square
-        value={this.state.squares[i]}
-        onClick={this.handleClick}
+        value={this.props.squares[i]}
+        onClick={this.props.onClick}
         squareNumber={i}
       />
     );
   }
 
   render() {
-    const winner = calculateWinner(this.state.squares);
-    let status = '';
-    if (winner) {
-      status = `Winner: ' ${winner}`;
-    } else {
-      status = `Next player: '  ${(this.state.xIsNext ? 'X' : 'O')}`;
-    }
     return (
       <div>
-        <div className={styles.status}>{status}</div>
         <div className={styles.boardRow}>
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -82,5 +55,9 @@ class Board extends Component {
   }
 }
 
-export default Board;
+Board.propTypes = {
+  squares: PropTypes.arrayOf(PropTypes.string),
+  onClick: PropTypes.func
+};
 
+export default Board;
--- tic-tac-toe/src/app/screens/Game/index.js
+++ tic-tac-toe/src/app/screens/Game/index.js
@@ -1,20 +1,81 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import styles from './styles.module.scss';
 import Board from './components/Board';
 
-function Game () {
-  return (
-    <div className={styles.game}>
-      <div className={styles.gameBoard}>
-        <Board />
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+class Game extends Component {
+  state = {
+    history: [{
+      squares: Array(9).fill(null)
+    }],
+    xIsNext: true
+  };
+
+  handleClick = i => {
+    const history = [...this.state.history];
+    const current = history[history.length - 1];
+    const squaresCopy = [...current.squares];
+    if (calculateWinner(squaresCopy) || squaresCopy[i]) {
+      return;
+    }
+    this.setState((prevState) => {
+      squaresCopy[i] = prevState.xIsNext ? 'X' : 'O';
+      return {
+        history: prevState.history.concat(
+          { squares: squaresCopy }
+        ),
+        xIsNext: !prevState.xIsNext
+      };
+    });
+  }
+
+  render() {
+    const history = [...this.state.history];
+    const current = history[history.length - 1];
+    const winner = calculateWinner(current.squares);
+
+    let status = '';
+    if (winner) {
+      status = `Winner: ${winner}`;
+    } else {
+      status = `Next player: ${(this.state.xIsNext ? 'X' : 'O')}`;
+    }
+
+    return (
+      <div className={styles.game}>
+        <div className={styles.gameBoard}>
+          <Board
+            squares={current.squares}
+            onClick={this.handleClick}
+          />
+        </div>
+        <div className={styles.gameInfo}>
+          <div>{ status }</div>
+          <ol>{/* TODO */}</ol>
+        </div>
       </div>
-      <div className={styles.gameInfo}>
-        <div>{/* status */}</div>
-        <ol>{/* TODO */}</ol>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Game;
